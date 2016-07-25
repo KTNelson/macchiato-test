91235497f31830711831d6e3962eb739c836311e
KTNelson <kierantnelson@gmail.com>
2016-07-25 06:16:39 +0100

    another tear down

--- macchiato/app/models/review.rb
+++
@@ -1,8 +0,0 @@
-class Review < ApplicationRecord
-  belongs_to :location, counter_cache: true
-  after_initialize :init
-
-    def init
-      self.result = 'True'
-    end
-end
--- macchiato/db/migrate/20160724141027_remove_score_from_location.rb
+++
@@ -1,5 +0,0 @@
-class RemoveScoreFromLocation < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :locations, :score, :integer
-  end
-end
